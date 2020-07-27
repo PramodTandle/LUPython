ffcfdc31d075f3545ddc6f94cbba18b35d601df2
PramodTandle <66490519+PramodTandle@users.noreply.github.com>
2020-07-28 02:07:06 +0530

    Add files via upload

---
+++ day6AssignmentPy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+#Day: 6 ,  Assignment 1
+
+list1=[1,2,3,4,5,7,8]
+list2=["a","b","c","d","e"]
+len1=min(len(list1), len(list2))
+dict1={}
+print({  list1[i] :list2[i]  for i in range(len1)  })
+
+
+# In[ ]:
+
+
+
+
